c811929d8f9fd9de13031fc422d2dc9a36a445a4
erez <erezshwarts1@msn.com>
2021-03-05 21:45:51 +0200

    hyperparameters change

--- model.py
+++ model.py
@@ -163,5 +163,5 @@ class ForecasterQR(pl.LightningModule):
         self.log('val_loss', loss, on_step=False, on_epoch=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
--- train.py
+++ train.py
@@ -10,18 +10,24 @@ from model import ForecasterQR
 data_path = os.path.join("data", "LD2011_2014.txt")
 
 if __name__ == '__main__':
-    train_el_dataloader, val_el_dataloader = DatasetHandler(data_path, num_samples=100, hist_hours=168, pred_horizon=24, batch_size=32).load_dataset()
+    train_el_dataloader, val_el_dataloader = DatasetHandler(
+        data_path,
+        num_samples=100,
+        hist_hours=168,
+        pred_horizon=24,
+        batch_size=128).load_dataset()
     quantiles = [.1, .2, .3, .4, .5, .6, .7, .8, .9, .95]
     model = ForecasterQR(
         x_dim=0,
         y_dim=1,
         input_max_squence_len=168,
-        encoder_hidden_dim=8,
+        encoder_hidden_dim=80,
         encoder_num_layers=1,
-        decoder_context_dim=4,
+        decoder_context_dim=40,
         quantiles=quantiles,
         horizons=24,
         device="gpu")
+
     trainer = pl.Trainer(gpus=1)
     trainer.fit(model, train_el_dataloader, val_el_dataloader)
 
